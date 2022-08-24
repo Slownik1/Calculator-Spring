db86e8535bc7da52fca4b7f4e2ef0db2b8fdac0c
dawid białek <dawid1bialek@gmail.com>
2022-08-24 22:40:22 +0200

    Initial commit

---
+++ src/main/java/com/example/Calculator/Spring/CalculatroRepository.java
@@ -0,0 +1,7 @@
+package com.example.Calculator.Spring;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CalculatroRepository {
+}
---
+++ src/main/java/com/example/Calculator/Spring/CalculatrorService.java
@@ -0,0 +1,7 @@
+package com.example.Calculator.Spring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatrorService {
+}
