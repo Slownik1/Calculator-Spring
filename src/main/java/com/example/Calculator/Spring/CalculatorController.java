2eb273a355cc0ee149a46b31ae6d973670b2ca22
dawid białek <dawid1bialek@gmail.com>
2022-08-25 22:18:57 +0200

    Add 3 new methods and endpoints for divide, multiply and minus operation

--- src/main/java/com/example/Calculator/Spring/CalculatorController.java
+++ src/main/java/com/example/Calculator/Spring/CalculatorController.java
@@ -2,7 +2,6 @@ package com.example.Calculator.Spring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CalculatorController {
 
-    private CalculatorService calculatorService;
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
 
     @GetMapping("/add")
-    public void add(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
-        System.out.println(a+b);
+    public int add(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
+        return calculatorService.add(a, b);
+    }
+
+    @GetMapping("/minus")
+    public int minus(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
+        return calculatorService.minus(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
+        return calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public int divide(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b){
+        if(a!=0) {
+            return calculatorService.divide(a, b);
+        }
+        else{
+            log.error("CAN NOT DIVIDE BY 0 !!!");
+            return 0;
+        }
     }
 
 }
--- src/main/java/com/example/Calculator/Spring/CalculatorService.java
+++ src/main/java/com/example/Calculator/Spring/CalculatorService.java
@@ -9,4 +9,15 @@ public class CalculatorService {
         return a+b;
     }
 
+    public int minus(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
 }
