0830762d110bea1bffd990b58cbdbc624d12c874
dawid białek <dawid1bialek@gmail.com>
2022-09-15 23:05:50 +0200

    new thymleaf template add.html

--- src/main/java/com/example/Calculator/Spring/CalculatorController.java
+++ src/main/java/com/example/Calculator/Spring/CalculatorController.java
@@ -2,6 +2,7 @@ package com.example.Calculator.Spring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class CalculatorController {
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
+    }
+
+    @RequestMapping(value="/add")
+    public String add(){
+        return"add";
     }
 
     @GetMapping("/add")
